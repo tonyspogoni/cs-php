58bb532e4bf5cc667a4bd40d15865ac7dd6ed51c
John Krieger <Johnkkrieger@gmail.com>
2016-05-19 20:58:58 -0600

    Fetches my database'

---
+++ databasegrab.php
@@ -0,0 +1,66 @@
+<?php
+	/*Don't forget to create a user for your database
+	CREATE USER 'steve'@'localhost' IDENTIFIED BY 'nerdface';
+	GRANT INSERT, SELECT, UPDATE, DELETE ON movies.* TO 'steve'@'localhost';
+	flush privileges
+	*/
+	$dbHost = "";
+	$dbPort = "";
+	$dbUser = "";
+	$dbPassword = "";
+
+    $dbName = 'Pokemon';
+
+     $openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
+	if ($openShiftVar === null || $openShiftVar == "")
+	{
+     // Not in the openshift environment
+    $dbHost = "localhost";
+	$dbHost = 'localhost';
+	$dbUser = 'steve';
+	$dbPassword = 'nerdface';
+	}
+	else
+	{
+    // In the openshift environment
+    $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+    $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+	$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+	$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+	}
+
+	try {
+		$db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
+	} catch (Exception $e) {
+		echo $e;
+		die();
+	}
+	?>
+
+<!DOCTYPE html>
+<html>
+<body>
+	<h1>Pokemon</h1>
+	<ul>
+		<?php
+		$stmt = $db->prepare('SELECT title from movie where id=:id');
+		$stmt->bindValue(':id', 2, PDO::PARAM_INT);
+		$stmt->execute();
+		$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		echo "<h3>Using the statement thing </h3>";
+		foreach ($rows as $row)
+		{
+		echo '<li>' . $row['title'] . "</li><br />";
+		}
+
+		echo "<h3>All data in movie table</h3>";
+		foreach ($db->query('SELECT * FROM movie') as $row)
+		{
+		echo '<li>' . $row['title'] . "</li><br />";
+		}
+
+		?>
+	<ul>
+
+</body>
+</html>

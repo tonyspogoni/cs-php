bf82224c57f7e4500b5339d71e2831224f414693
John Krieger <Johnkkrieger@gmail.com>
2016-05-30 14:40:29 -0600

    Databse adding code

--- addPokemon.php
+++ addPokemon.php
@@ -1,7 +1,45 @@
 
 <?php
-require("databasegrab.php");
-$db = connectToDb();
+
+/*Don't forget to create a user for your database
+CREATE USER 'steve'@'localhost' IDENTIFIED BY 'nerdface';
+GRANT INSERT, SELECT, UPDATE, DELETE ON movies.* TO 'steve'@'localhost';
+flush privileges
+*/
+$dbHost = "";
+$dbPort = "";
+$dbUser = "";
+$dbPassword = "";
+
+$dbName = 'pokemon';
+
+$openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
+if ($openShiftVar === null || $openShiftVar == "")
+{
+	// Not in the openshift environment
+	$dbHost = "localhost";
+	$dbHost = 'localhost';
+	$dbUser = 'steve';
+	$dbPassword = 'nerdface';
+}
+else
+{
+	// In the openshift environment
+	$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+	$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+	$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+	$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+}
+
+try {
+	$db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
+} catch (Exception $e) {
+	echo $e;
+	die();
+}
+
+//require("databasegrab.php");
+//$db = connectToDb();
 
 $name = htmlspecialchars($_POST['name']);
 $type = htmlspecialchars($_POST['type']);

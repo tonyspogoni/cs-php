44c26aa956b39b1e301775a32efd6082db12d3e0
John Krieger <Johnkkrieger@gmail.com>
2016-05-30 14:38:20 -0600

    Databse adding code

--- addPokemon.php
+++ addPokemon.php
@@ -11,13 +11,9 @@ $stmt = $db->prepare($query);
 $stmt->bindValue(":name", $name, PDO::PARAM_STR);
 $stmt->bindValue(":pokedexNum", $pokedexNum, PDO::PARAM_INT);
 $stmt->execute();
-//$query2 = "INSERT INTO Type(TYPE) VALUES (:type)";
-//$stmt2 = $db->prepare($query2);
-//$stmt2->bindValue(":type", $type, PDO::PARAM_STR);
-//$stmt2->execute();
 echo "got er done";
-//header("Location: databasegrab.php");
-//die("Page should have been redirected");
+header("Location: databasegrab.php");
+die("Page should have been redirected");
 
 
 ?>

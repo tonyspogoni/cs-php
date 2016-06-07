552aea3d179089260965add08ea01de1a946f229
John Krieger <Johnkkrieger@gmail.com>
2016-06-07 00:15:58 -0600

    Just a few changes

--- addPokemon.php
+++ addPokemon.php
@@ -50,19 +50,7 @@ $stmt->bindValue(":name", $name, PDO::PARAM_STR);
 $stmt->bindValue(":type", $type, PDO::PARAM_STR);
 $stmt->bindValue(":pokedexNum", $pokedexNum, PDO::PARAM_INT);
 $stmt->execute();
-$query = "SELECT t.ID, p.ID from Pokemon p
-INNER JOIN Type t on pt.POKEMON_TYPE = t.ID
-INNER JOIN Pokemon_Type pt on pt.POKEMON_ID = t.ID
-WHERE t.TYPE = :type AND  p.Name = :name";
-$stmt->bindValue(":name", $name, PDO::PARAM_STR);
-$stmt->bindValue(":type", $type, PDO::PARAM_STR);
-$stmt = $db->query($query);
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
-$typeid = $row['t.ID'];
-$pokemonID = $row['p.ID'];
-$query = "INSERT INTO Pokemon_Type (TYPE_ID,POKEMON_ID) VALUES ($typeId, $pokemonId)";
-$stmt = $db->query($query);
-$stmt->execute();
+
 //$query2 = "INSERT INTO Pokemon_Type (TYPE_ID,POKEMON_ID) SELECT t.ID, p.ID from Pokemon p INNER JOIN Type t on pt.TYPE_ID = t.ID INNER JOIN Pokemon_Type pt on pt.POKEMON_ID = p.ID WHERE p.Name = :name AND t.TYPE = :type ";
 //$stmt2 = $db->prepare($query2);
 
